58580ea8898de34b95321b876638c5675c1e3087
Jose Angel <jangel.romero@gmail.com>
2017-11-23 18:52:36 +0100

    Config files

--- DBSchema/dump-mysql.php
+++ DBSchema/dump-mysql.php
@@ -3,19 +3,38 @@
  * Put a 'pre-commit' file in '.git/hooks/'
  *
  * 'pre-commit' file:
-------------------
-php "DBSchema/dump-mysql.php"
-exit 0
-------------------
+ * ------------------
+ * php "DBSchema/dump-mysql.php"
+ * exit 0
+ * ------------------
  */
 
+//--------
+$DOCKER_DIR = "docker-membrillo";
 $DBNAME = "membrillo2";
 $DBUSER = "root";
 $DBPASS = "root";
 
-$SCHEMAPATH = "./DBSchema";
-$DB_FILE = $SCHEMAPATH."/".$DBNAME."-".$_SERVER['COMPUTERNAME'].".sql";
+//--------
+$current_dir = getcwd();
+$DB_FILE_CREATE = $current_dir."/DBSchema/create-".$DBNAME.".sql";
+$DB_FILE_DATA   = $current_dir."/DBSchema/data-"  .$DBNAME.".sql";
+
+//--------
+$only_creates = "--no-data --skip-add-drop-table ";
+$only_data    = "--no-create-info ";
+$dump_command = "--skip-comments -u $DBUSER -p$DBPASS $DBNAME | sed 's/ AUTO_INCREMENT=[0-9]*\b//'";
+
+$dump_command_create = "mysqldump ".$only_creates.$dump_command." > $DB_FILE_CREATE";
+$dump_command_data   = "mysqldump ".$only_data   .$dump_command." > $DB_FILE_DATA";
+
+//--------
+chdir($DOCKER_DIR);
+exec("docker-compose exec mysql ".$dump_command_create);
+exec("docker-compose exec mysql ".$dump_command_data);
+
+//---------
+chdir($current_dir);
+exec("git add $DB_FILE_CREATE $DB_FILE_DATA");
 
 # mysqldump --no-data --skip-add-drop-table --skip-comments -u $DBUSER -p$DBPASS $DBNAME > $DBFILE.sql
-system("mysqldump --no-data --skip-add-drop-table --skip-comments -u $DBUSER $DBNAME | sed 's/ AUTO_INCREMENT=[0-9]*\b//' > $DB_FILE");
-system("git add $DB_FILE");
